5e0d2dd4452db0ae89cc06688cd3575dd817b093
Jacob_str <80145275+Ghdhdhdh@users.noreply.github.com>
2023-03-04 18:18:23 +1100

    Create main.sh

---
+++ sudo userdel ug0thack3d sudo adduser courtney sudo groupadd emeritusmentor sudo adduser sam emeritusmentor sudo deluser sam sudo sudo adduser chris sudo cd/home/yang/&& rm -rf clickonthistomakeyourdream.dll  sudo find/main.sh
@@ -0,0 +1,8 @@
+sudo userdel ug0thack3d
+sudo adduser courtney
+sudo groupadd emeritusmentor
+sudo adduser sam emeritusmentor
+sudo deluser sam sudo
+sudo adduser chris sudo
+cd /home/yang/ && rm -rf clickonthistomakeyourdream.dll 
+sudo find / -name "thisisabackdoor.exe" -delete
